be5a29acd6b473c36594bf0b8ac6a1b9e7509adb
VctrMndz <65990008+VctrMndz@users.noreply.github.com>
2020-06-24 22:43:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,26 +18,26 @@ sr.reveal('.animate-left', {
     origin: 'left',
     duration: 1000,
     distance: '25rem', 
-    delay:600
+    delay:200
 });
 
 sr.reveal('.animate-right', {
     origin: 'right',
     duration: 1000,
     distance: '25rem', 
-    delay:600
+    delay:200
 });
 
 sr.reveal('.animate-top', {
     origin: 'top',
     duration: 1000,
     distance: '25rem', 
-    delay:600
+    delay:200
 });
 
 sr.reveal('.animate-bottom', {
     origin: 'bottom',
     duration: 1000,
     distance: '25rem', 
-    delay:600
+    delay:200
 });
